7be7993b2fcabb3d8cfbcaa9b127dfabea74223e
Luca Guidi <me@lucaguidi.com>
2016-09-21 17:24:24 +0200

    Make ROM::Sql::Types::Decimal to work with floats

--- lib/rom/sql/types.rb
+++ lib/rom/sql/types.rb
@@ -10,11 +10,11 @@ module ROM
       String   = ROM::Types::Optional::Coercible::String
       Int      = ROM::Types::Optional::Coercible::Int
       Float    = ROM::Types::Optional::Coercible::Float
-      Decimal  = ROM::Types::Optional::Coercible::Decimal
       Array    = ROM::Types::Optional::Coercible::Array
       Hash     = ROM::Types::Optional::Coercible::Hash
 
       Bool     = ROM::Types::Bool.optional
+      Decimal  = Types::Decimal.constructor(->(decimal) { ::BigDecimal.new(decimal, ::BigDecimal.double_fig) }).optional
       Date     = ROM::Types::Date.constructor(->(date) { ::Date.parse(date.to_s) }).optional
       Time     = ROM::Types::Time.constructor(->(time) { ::Time.parse(time.to_s) }).optional
       DateTime = ROM::Types::DateTime.constructor(->(datetime) { ::DateTime.parse(datetime.to_s) }).optional
